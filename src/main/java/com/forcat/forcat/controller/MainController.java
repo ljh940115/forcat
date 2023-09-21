d1ecfcde3c426b270e8f6989c5a9b60c248bd3af
10-5\kkw <ljh940115@naver.com>
2023-09-21 17:55:16 +0900

    1조 9월 21일 프로젝트 첫 시작... 학원 끝나기 전에 올림

--- src/main/java/com/forcat/forcat/controller/MainController.java
+++ src/main/java/com/forcat/forcat/controller/MainController.java
@@ -1,6 +1,7 @@
 package com.forcat.forcat.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -11,6 +12,12 @@ public class MainController {
     @GetMapping("/index")
     public String index(){
         return "index";
+    }
+
+    @GetMapping("/test")
+    public String test(Model model){
+        model.addAttribute("data", "타임리프 테스트");
+        return "thymeleafEx";
     }
 
 }
